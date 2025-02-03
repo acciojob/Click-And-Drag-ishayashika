6b5120a29565125e284332241d6670d18ab7caf8
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-02-03 14:39:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Your code here.
+let dragEle=null;
+document.querySelectorAll('.item').forEach((item)=>{
+	item.setAttribute("draggable", true);
+	item.addEventListener("dragstart",(e)=>{
+		dragEle=e.target;
+	});
+});
+
+document.querySelector('.items').addEventListener("dragover",(e)=>{
+		e.preventDefault();
+});
+
+document.querySelector('.items').addEventListener("drop",(e)=>{
+		e.preventDefault();
+		document.querySelector('.items').appendChild(dragEle);
+		dragEle=null;
+});
